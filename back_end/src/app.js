08d8c207acf7b26a314cef219c5dd0614258c982
otaviozerotwo <deadplatypus@protonmail.com>
2023-10-22 21:33:06 -0300

    adição biblioteca cors

--- back_end/src/app.js
+++ back_end/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
+app.use(cors());
 app.use(router);
 
 module.exports = app;
