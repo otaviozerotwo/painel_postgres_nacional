8c3aafa95cad2cbec90a992ae8bbad74e845f681
otaviozerotwo <deadplatypus@protonmail.com>
2023-10-24 23:09:14 -0300

    ajustes função de validação da procedure de descontos

--- back_end/src/models/pdvModel.js
+++ back_end/src/models/pdvModel.js
@@ -4,7 +4,7 @@ const formatDate = (date) => {
     const day = String(date.getDate()).padStart(2, '0'); // Adiciona zero à esquerda, se necessário
   
     return `${year}/${month}/${day}`;
-  }
+  };
   
 const dataAtual = new Date();
 const dataFormatada = [formatDate(dataAtual)];
@@ -19,15 +19,28 @@ const getDadosFilial = async (connection) => {
     const sql = `SELECT cd_filial FROM prc_filial`;
     const consulta = await connection.query(sql);
     return consulta;
-}
+};
 
-const getProcedureDescontos = async (connection) => {
-    const codigoFilial = [1];
-    // const codigoProduto = [563780];
-    const sql = `SELECT cd_tbl_desc, ds_desconto, * FROM p_est_prod_descontos(1,$1,563780,0 , '0', 0, 1 , 1, 0, 0, 0,0,0);`;
-    const consulta = await connection.query(sql, codigoFilial);
+const getProcedureDescontos = async (connection, codigoFilial) => {
+    const sql = `SELECT cd_tbl_desc, ds_desconto, * FROM p_est_prod_descontos($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13);`;
+    const parametros = [
+        codigoEmpresa = 1,
+        codigoFilial,
+        codigoProduto = 80036,
+        codigoConvenio = 0,
+        cartaoFidelidade = '0',
+        ean = 0,
+        codigoCliente = 0,
+        qtdProduto = 1,
+        aplicacao = 0,
+        idPromo = 0,
+        seReceita = 0,
+        codigoFormaPagto = 0,
+        validaStatusCred = 0
+    ]
+    const consulta = await connection.query(sql, parametros);
     return consulta;
-}
+};
 
 module.exports = {
     getQtdCuponsFilial,
